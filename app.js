401ad7a0fe31f59595ef88181be92e9fc651301d
DeepakYadav <42115559+deepak-07@users.noreply.github.com>
2021-01-18 23:57:50 +0530

    feat: palindrome loop added

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // alert("lo mai aa gya");
 var date = document.querySelector("#dob");
+
+
+
+
+var rem = 0;
+var reverse;
+var number = date;
+while(number!=0){
+
+    rem = number % 10;
+    reverse = rem*10 + rem;
+    number = number /10;
+}
+console.log(number);
+
