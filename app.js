8523e9e256bb297450e3b40b4d12fb1b2155328d
DeepakYadav <42115559+deepak-07@users.noreply.github.com>
2021-01-20 23:34:33 +0530

    fix: checking date

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-// alert("lo mai aa gya");
-var date = document.querySelector("#dob");
-
-
+alert("lo mai aa gya");
+var date = document.querySelector("#birthday").value;
+var btn = document.querySelector(".check-btn");
+var output = document.querySelector(".outputArea");
 
 
 var rem = 0;
-var reverse;
+var reverse = 0;
 var number = date;
-while(number!=0){
+console.log(date);
 
-    rem = number % 10;
-    reverse = rem*10 + rem;
-    number = number /10;
+function clickHandler() {
+    while (number != 0) {
+
+        rem = number % 10;
+        reverse = rem * 10 + rem;
+        number = number / 10;
+    }
+    console.log(number);
+    output.innerHTML = date;
 }
 console.log(number);
-
+btn.addEventListener("click", clickHandler);
